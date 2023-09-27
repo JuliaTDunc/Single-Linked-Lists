98a7471b1f1b74bca2645bf1aec16816b3e02261
Julia Duncan <juliataylorduncan@gmail.com>
2023-09-26 20:10:40 -0700

    Complete

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -8,15 +8,33 @@ class DoublyLinkedListNode {
 
 class DoublyLinkedList {
   constructor() {
-    // Your code here
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
   addToHead(val) {
-    // Your code here
+    let newNode = new DoublyLinkedListNode(val);
+    if(!this.length) {
+    this.head = newNode;
+    this.tail = newNode;
+    }
+    this.head.prev = newNode;
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
   }
 
   addToTail(val) {
-    // Your code here
+    let newNode = new DoublyLinkedListNode(val);
+    if(!this.length) {
+      this.tail = newNode;
+      this.head = newNode;
+    }
+    this.tail.next = newNode;
+    newNode.prev = this.tail;
+    this.tail = newNode;
+    this.length++;
   }
 
   // You can use this function to help debug
--- linked-list-timing.js
+++ linked-list-timing.js
@@ -6,4 +6,3 @@ Construct a timing test to verify the time complexities of `addToHead` and
 `addToTail` for both singly and doubly linked lists.
 */
 
-// Your code here
