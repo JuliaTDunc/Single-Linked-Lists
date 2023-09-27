c39d7775cbc233364ab290c30dae5187f2d58ca8
Julia Duncan <juliataylorduncan@gmail.com>
2023-09-26 19:49:29 -0700

    Pt1

--- linked-list.js
+++ linked-list.js
@@ -2,20 +2,41 @@ class LinkedListNode {
   constructor(val) {
     this.value = val;
     this.next = null;
+    
   }
 }
 
 class LinkedList {
   constructor() {
-    // Your code here
+    this.head = null;
+    this.length = 0;
   }
 
   addToHead(val) {
-    // Your code here
+    const newNode = new LinkedListNode(val)
+    if(!this.length) {
+      this.head = newNode;
+      this.length++
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+      this.length++;
+    }
+   
   }
 
   addToTail(val) {
-    // Your code here
+    let newNode = new LinkedListNode(val)
+    this.length++
+    if (!this.head) {
+      this.head = newNode;
+      return this;
+    } 
+    let currNode = this.head;
+    while (currNode.next) {
+        currNode = currNode.next;
+    }
+    currNode.next = newNode;
   }
 
   // You can use this function to help debug
